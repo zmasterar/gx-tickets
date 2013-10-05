aa22e2a6a84378b89f27eb0a7b0044528475c5b2
zmaster <benja@zmaster.com.ar>
2013-10-05 10:53:15 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" /> 
+  <title>Tickets</title>
+</head>
+<body>
+  <h1>Tickets</h1>
+Si querés hacer una denuncia por robo, extravío de cuenta o cualquier problema relacionado al juego que necesite la intervención de un GM/Admin, tenés que realizar un ticket en nuestro sistema de soporte:
+
+<center><h2><a href="http://soporte.gxzone.com" target"_blank">Hacé clic aquí para entrar al sistema de TICKETS</a></h2></center>
+
+Esto incluye..
+
+<ul><li>Denuncias</li>
+<li>Problemas de logueo (ban?)</li>
+<li>Cuentas Robadas</li>
+<li>Avisos al staff como por ejemplo bugs criticos encontrados</li>
+<li>Problemas con donaciones</li>
+<li>etc..</li></ul>
+
+<p>También podés ingresar al sistema de soporte haciendo click en el link SOPORTE en la parte superior de cualquier página de nuestro <a href="http://foros.gxzone.com/forum.php">foro</a> como en la siguiente imagen</p>
+
+<center><a href="http://soporte.gxzone.com" target"_blank"><img src="http://image.gxzone.com/images/8/5/85d9cc477a8.png"></a></center>
+</body>
+</html>
