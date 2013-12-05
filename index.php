e2039d2c2752f7f13171eba016443782fe293a72
Benjamín Lavena <benja@zmaster.com.ar>
2013-12-04 22:53:42 -0300

    Arreglo de target en links

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <h1>Tickets</h1>
 Si querés hacer una denuncia por robo, extravío de cuenta o cualquier problema relacionado al juego que necesite la intervención de un GM/Admin, tenés que realizar un ticket en nuestro sistema de soporte:
 
-<center><h2><a href="http://soporte.gxzone.com" target"_blank">Hacé clic aquí para entrar al sistema de TICKETS</a></h2></center>
+<center><h2><a href="http://soporte.gxzone.com" target="_blank">Hacé clic aquí para entrar al sistema de TICKETS</a></h2></center>
 
 Esto incluye..
 
@@ -18,8 +18,8 @@ Esto incluye..
 <li>Problemas con donaciones</li>
 <li>etc..</li></ul>
 
-<p>También podés ingresar al sistema de soporte haciendo click en el link SOPORTE en la parte superior de cualquier página de nuestro <a href="http://foros.gxzone.com/forum.php">foro</a> como en la siguiente imagen</p>
+<p>También podés ingresar al sistema de soporte haciendo click en el link SOPORTE en la parte superior de cualquier página de nuestro <a href="http://foros.gxzone.com/forum.php" target="_blank">foro</a> como en la siguiente imagen</p>
 
-<center><a href="http://soporte.gxzone.com" target"_blank"><img src="http://image.gxzone.com/images/8/5/85d9cc477a8.png"></a></center>
+<center><a href="http://soporte.gxzone.com" target="_blank"><img src="http://image.gxzone.com/images/8/5/85d9cc477a8.png"></a></center>
 </body>
 </html>
